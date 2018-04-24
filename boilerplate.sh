28c5472744678fb2364eeb4b44e94885e29ec62c
Erich Malfertheiner <erichmalfertheiner01@gmail.com>
2018-04-24 02:37:03 -0300

    upload files

---
+++ boilerplate.sh
@@ -0,0 +1,19 @@
+echo '<!DOCTYPE html>' >> new.html
+echo '<html>' >> new.html
+echo '<head>' >> new.html
+echo '<meta charset="utf-8">' >> new.html
+echo '<meta name="author" content="">' >> new.html
+echo '<meta name="description" content="Tl">' >> new.html
+echo '<meta name="keywords" content="">' >> new.html
+echo '<meta http-equiv="content-language" content="pt-br">' >> new.html
+echo '<meta http-equiv="Content-Type" content="text/html">' >> new.html
+echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">' >> new.html
+echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">' >> new.html
+echo '<title></title>' >> new.html
+echo '</head>' >> new.html
+echo '<br>' >> new.html
+echo '<body>' >> new.html
+echo '<br>' >> new.html
+echo '</body>' >> new.html
+echo '</html>' >> new.html
+echo '</html>'  >> new.html
